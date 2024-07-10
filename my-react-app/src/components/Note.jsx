14cd3adefa922717f2ce2fb59852b0ad0ee5faa8
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2024-07-10 13:05:37 -0400

    made some components

---
+++ my-react-app/src/components/Note.jsx
@@ -0,0 +1,10 @@
+function Note() {
+    return (
+        <div className="note">
+            <h1>React</h1>
+            <p>Learning about components</p>
+        </div>
+    )
+  }
+  
+export default Note;
